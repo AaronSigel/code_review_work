b1a4676a0a3203d10ece5c7c592105c2ae50a1d4
Dimitriy Shevchenko <dshevchenko@mail.com>
2024-12-12 14:18:42 +0300

    update calc

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
@@ -14,21 +13,32 @@ class CalculatorTest {
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        Calculator calc = new Calculator();
+        assertEquals(3, calc.dif(5, 2));
+        assertEquals(-2, calc.dif(1, 3));
+        assertEquals(0, calc.dif(7, 7));
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        Calculator calc = new Calculator();
+        assertEquals(2, calc.div(10, 5));
+        assertEquals(-2, calc.div(-10, 5));
+        assertThrows(ArithmeticException.class, () -> calc.div(10, 0));
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        Calculator calc = new Calculator();
+        assertEquals(6, calc.times(2, 3));
+        assertEquals(0, calc.times(0, 10));
+        assertEquals(-15, calc.times(-3, 5));
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        Calculator calc = new Calculator();
+        // Предположим, что solver выполняет ( (5+3)*(10-2) ) / 4 = (8*8)/4 = 16
+        assertEquals(16, calc.solver());
     }
 }
